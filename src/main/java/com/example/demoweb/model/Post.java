4c2a6a772240ed72d9e36193e08e7d2f4e27df54
DemLoveSky <dem.loginov@gmail.com>
2023-02-20 05:32:57 +0500

    .

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -4,19 +4,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Post {
+
+    private Long id;
     private String text;
     private Integer likes;
     private Date creationDate;
 
-    public Post(String _text, Date _createDate) {
-        this(_text, _createDate, 0);
+    public Post(Long _id, String _text, Date _createDate) {
+        this(_id, _text, _createDate, 0);
     }
 
-    public Post(String _text, Date _createDate, Integer _likes) {
+    public Post(Long _id, String _text, Date _createDate, Integer _likes) {
+        this.id = _id;
         this.text = _text;
         this.creationDate = _createDate;
         this.likes = _likes;
     }
+
+    public Long getId() { return this.id; }
 
     public void setLikes(int likes) {
         this.likes = likes;
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -12,13 +12,13 @@ public class PostService {
     ArrayList<Post> posts = new ArrayList<Post>();
 
     public PostService() {
-        posts.add(new Post("В Африканском союзе объяснили, почему делегацию Израиля выгнали с саммита",
+        posts.add(new Post((long) posts.size(), "В Африканском союзе объяснили, почему делегацию Израиля выгнали с саммита",
                 new Date(2454545564L),
                 34));
-        posts.add(new Post("В Вашингтоне началась акция против поддержки Украины",
+        posts.add(new Post((long) posts.size(), "В Вашингтоне началась акция против поддержки Украины",
                 new Date(245456766564L),
                 54));
-        posts.add(new Post("Пекин отказался мириться с попытками США надавить на отношения России и КНР",
+        posts.add(new Post((long) posts.size(), "Пекин отказался мириться с попытками США надавить на отношения России и КНР",
                 new Date(6576743564L),
                 57));
     }
@@ -28,6 +28,6 @@ public class PostService {
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        posts.add(new Post((long) posts.size(), text, new Date()));
     }
 }
